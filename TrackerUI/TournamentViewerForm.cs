949d8b9aa3397a06fd42b1286fd99b50f1738d68
andrija.pecaric@gmail.com <andrija.pecaric@gmail.com>
2023-08-21 19:53:50 +0200

    update

--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using TrackerLibrary.Models;
 using Windows.Networking.Sockets;
 
@@ -6,13 +7,16 @@ namespace TrackerUI
     public partial class TournamentViewerForm : Form
     {
         private TournamentModel _tournament;
-        List<int> rounds = new List<int>();
-        List<MatchupModel> selectedMatchups = new List<MatchupModel>();
+        BindingList<int> rounds = new BindingList<int>();
+        BindingList<MatchupModel> selectedMatchups = new BindingList<MatchupModel>();
+
         public TournamentViewerForm(TournamentModel tournamentModel)
         {
             InitializeComponent();
 
             _tournament = tournamentModel;
+
+            WireUpLists();
 
             LoadFormData();
 
@@ -24,85 +28,96 @@ namespace TrackerUI
             tournamentNameLabel.Text = _tournament.TournamentName;
         }
 
-        private void WireUpRoundsList()
-        {
-            roundComboBox.DataSource = null;
+        private void WireUpLists()
+        {          
             roundComboBox.DataSource = rounds;
-        }
-
-        private void WireUpMatchupsList()
-        {
-            matchupListBox.DataSource = null;
             matchupListBox.DataSource = selectedMatchups;
             matchupListBox.DisplayMember = "DisplayName";
         }
+
         private void LoadRounds()
         {
-            rounds = new List<int>();
+            rounds.Clear();
 
             rounds.Add(1);
             int currRound = 1;
 
             foreach (List<MatchupModel> matchups in _tournament.Rounds)
             {
-                if (matchups.Any() && matchups.First().MatchupRound > currRound)
+                if (matchups.First().MatchupRound > currRound)
                 {
                     currRound = matchups.First().MatchupRound;
                     rounds.Add(currRound);
                 }
             }
 
-            WireUpRoundsList();
+            LoadMatchups(1);
         }
 
         private void roundComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            LoadMatchups();
+            LoadMatchups((int)roundComboBox.SelectedItem);
         }
-        // debugg not showin listBox!! less22(40min)
-        private void LoadMatchups()
+        // debugg nullexception m.Entries, less23;
+        private void LoadMatchups(int round)
         {
-
-            int round = (int)roundComboBox.SelectedItem;
-
             foreach (List<MatchupModel> matchups in _tournament.Rounds)
             {
 
-                if (matchups.Count > 0 && matchups.First().MatchupRound == round)
+                if (matchups.First().MatchupRound == round)
                 {
-                    selectedMatchups = matchups;
+                    selectedMatchups.Clear();
+                    foreach (MatchupModel m in matchups)
+                    {
+                        selectedMatchups.Add(m);
+                    }
                 }
-
             }
 
-            WireUpMatchupsList();
+            LoadMatchup(selectedMatchups.First());
         }
 
-        private void LoadMatchup()
+        private void LoadMatchup(MatchupModel m)
         {
-            MatchupModel m = (MatchupModel)matchupListBox.SelectedItem;
-
+            
             for (int i = 0; i < m.Entries.Count; i++)
             {
                 if (i == 0)
-                { 
+                {
                     if (m.Entries[0].TeamCompeting != null)
                     {
-                        teamOneLabel.Text = m.Entries[0].TeamCompeting.TeamName;
+                        teamOneLabel.Text = m.Entries[0].TeamCompeting?.TeamName;
                         teamOneScoreTextBox.Text = m.Entries[0].Score.ToString();
+
+                        teamTwoLabel.Text = "<bye>";
+                        teamTwoScoreTextBox.Text = "0";
                     }
                     else
                     {
                         teamOneLabel.Text = "Not yet set";
+                        teamOneScoreTextBox.Text = "";
                     }
+                }
 
+                if (i == 1)
+                {
+                    if (m.Entries[1].TeamCompeting != null)
+                    {
+                        teamTwoLabel.Text = m.Entries[1].TeamCompeting?.TeamName;
+                        teamTwoScoreTextBox.Text = m.Entries[1].Score.ToString();
+                    }
+                    else
+                    {
+                        teamTwoLabel.Text = "Not yet set";
+                        teamTwoScoreTextBox.Text = "";
+                    }
                 }
             }
         }
 
         private void matchupListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            LoadMatchup();
+            LoadMatchup((MatchupModel)matchupListBox.SelectedItem);
         }
     }
 }
